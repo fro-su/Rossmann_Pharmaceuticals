282ddf00d62522dfd3f1be3abdfbcde4bed4b5ad
Firew Ayele <110311306+fro-su@users.noreply.github.com>
2024-11-21 15:12:43 +0300

    Create test data

---
+++ tests/unittest.py
@@ -0,0 +1,40 @@
+import unittest
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from src.utils import *
+
+class TestDataAnalysisFunctions(unittest.TestCase):
+
+    def setUp(self):
+        # Create test data
+        self.df = pd.DataFrame({
+            'A': [1, 2, None, 4],
+            'B': [None, 2, 3, 4],
+            'C': ['x', 'y', 'z', 'w'],
+            'D': [1, 2, 2, 4]
+        })
+
+        self.duplicate_df = pd.DataFrame({
+            'ID': [1, 2, 2, 3],
+            'Value': [10, 20, 20, 30]
+        })
+
+        self.numeric_df = pd.DataFrame({
+            'A': [1, 2, -3, 4],
+            'B': [100, 200, 300, 400]
+        })
+
+        self.date_df1 = pd.DataFrame({
+            'Date1': ['2024-01-01', '2024-01-02', '2024-01-03'],
+            'Value1': [10, 20, 30]
+        })
+
+        self.date_df2 = pd.DataFrame({
+            'Date2': ['2024-01-02', '2024-01-03', '2024-01-04'],
+            'Value2': [100, 200, 300]
+        })
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
